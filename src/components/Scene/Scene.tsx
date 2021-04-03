c3a0334f1ebd3480269d0f1be0d3fe86b3794140
Daniel Corner <danielcorner7@gmail.com>
2021-04-03 00:51:46 -0400

    basic examples

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,36 +1,73 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Physics } from "@react-three/cannon";
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls, RoundedBox } from "@react-three/drei";
 import { Lighting } from "./Lighting";
 import { useAudioTrack } from "../music/useAudioTrack";
-import { a } from "react-spring/three";
+import { animated, useSpring } from "react-spring/three";
+import { useControl } from "react-three-gui";
 
 const Scene = () => {
   // useCameraWobble();
+  useAudioTrack();
+
   return (
     <Suspense fallback={null}>
-      <AudioTrack />
       <OrbitControls />
-      <PhysicsSceneMovable />
+      <PhysicsScene />
       <Lighting />
     </Suspense>
   );
 };
 
-function PhysicsSceneMovable() {
+/** contains objects affected by physics */
+function PhysicsScene() {
+  const [sprung, setSprung] = useState(false);
+
+  // https://codesandbox.io/s/react-spring-v9-rc-6hi1y?file=/src/index.js:983-1012
+  // set up a spring to bounce from 0 to 1
+  // set the stored value based on this progress %
+  const { position } = useSpring({
+    // progress: Number(sprung),
+    position: sprung ? [0, 0, 2] : [0, 0, 0],
+    config: { tension: 170, mass: 1, friction: 17 },
+    // onChange({ progress }) {
+    //   set({ [property]: firstValue + delta * progress });
+    // },
+  });
+
   return (
-    <a.group>
-      <Physics>
-        <mesh />
-      </Physics>
-    </a.group>
+    <Physics>
+      <BigRedButton
+        onClick={() => setSprung((prev) => !prev)}
+        position={[0, 1, 0]}
+      />
+      <animated.group position={position}>
+        <Box
+          args={[1, 1, 1]} // Width, Height and Depth of the box
+        />
+      </animated.group>
+      <animated.mesh>
+        <Box args={[2, 2, 2]} position={[-5, -5, -5]} />
+      </animated.mesh>
+    </Physics>
   );
 }
 
-function AudioTrack() {
-  // audio track
-  useAudioTrack();
-  return null;
+function BigRedButton({ onClick, children = null, ...rest }) {
+  const radius = useControl("radius", { type: "number" });
+
+  return (
+    <RoundedBox
+      args={[1, 1, 1]} // Width, Height and Depth of the box
+      radius={radius} // Border-Radius of the box
+      smoothness={4} // Optional, number of subdivisions
+      {...rest} // All THREE.Mesh props are valid
+      onClick={onClick}
+    >
+      <meshBasicMaterial color="red" />
+      {children}
+    </RoundedBox>
+  );
 }
 
 // PROTEINS.forEach(({ pathToGLTF }) => // useGLTF.preload(pathToGLTF));
